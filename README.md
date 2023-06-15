deae007cfc7afa15b0c4ca62d0c7636e981646f2
dlchen <dan.zot@gmail.com>
2023-06-15 13:56:34 -0700

    rename to jigglypuff

--- README.md
+++ README.md
@@ -1,12 +1,4 @@
-# robo-jiggly
-
-Stay awake by mouse movement.
-
-Default movement interval is a random duration between 1 to 10 minutes.
-
-Default movement amount is 1 pixel.
-
-Does not handle case when mouse is at the edge of the screen.
+# jigglypuff
 
 ## Usage
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+/**
+ * Stay awake by mouse movement.
+ * Default movement interval is a random duration between 1 to 10 minutes.
+ * Default movement amount is 1 pixel.
+ * Does not handle case when mouse is at the edge of the screen.
+ */
+
 const robot = require('robotjs');
 
 const { logger } = require('./utils')
